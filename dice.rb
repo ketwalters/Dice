98b68966b0f9649b1ea57dd82572f2b20f2aadeb
Keturah Walters <your.ketwalters@gmail.com>
2013-11-13 16:10:15 -0500

    Add dice testing folder

---
+++ dice.rb
@@ -0,0 +1,16 @@
+# Create a method in the dice class called `roll` that returns a random number
+# between 1 and 6
+
+#To test your solution run `rspec dice_spec.rb` in your terminal
+
+class Dice
+
+def roll
+array = [1,2,3,4,5,6]
+array [rand(6)]
+
+  end
+end
+
+black = Dice.new
+black.roll
